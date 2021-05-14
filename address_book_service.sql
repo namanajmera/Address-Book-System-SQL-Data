3113c5087eef7ba761ffd5654ed1f59770b4bf26
Naman <cool.naman.ajmera@gmail.com>
2021-05-14 09:00:45 +0530

    Added: Ability to create a table.

--- address_book_service.sql
+++ address_book_service.sql
@@ -4,3 +4,19 @@ show databases;
 create database address_book_service;
 
 use address_book_service;
+
+#UC2
+create table address_book
+(
+	id int unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	f_name varchar(100) NOT NULL,
+	l_name varchar(100) NOT NULL,
+	address varchar(250) NOT NULL,
+	city varchar(100) NOT NULL,
+	state varchar(100) NOT NULL,
+	zip int NOT NULL,
+	phone_number varchar(12) NOT NULL,
+   	email varchar(100) NOT NULL UNIQUE
+);
+
+desc address_book;
